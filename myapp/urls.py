1ae262988f77b9795b05c3f2e7cfb333e220e2f1
ShahadAlkamli <85537889+ShahadAlkamli@users.noreply.github.com>
2026-09-23 23:21:37 +0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,12 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from myapp import views  # Import your views from the app
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
+from myapp import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('api/transactions/', views.TransactionListCreate.as_view(), name='transaction-list'),
     path('api/transactions/<int:pk>/', views.TransactionDetail.as_view(), name='transaction-detail'),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),  # Use Django's built-in LogoutView
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
